0c60d0be3b8b9553aba95f033c86b43f58364e63
Wang Zixiang <1852232090@qq.com>
2025-12-18 00:09:46 +0800

    完成到hw08

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -1,7 +1,39 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (cond ((null? s) #t)
+        ((null? (cdr s)) #t)
+        ((<= (car s) (car(cdr s))) (ascending? (cdr s)))
+        (else #f))
+)
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+  (cond ((null? s) '())
+        ((pred (car s))
+          (cons (car s) (my-filter pred (cdr s))))
+        (else (my-filter pred (cdr s)))
+  )
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+  (cond ((null? lst1) lst2)
+        ((null? lst2) lst1)
+        (else
+          (cons (car lst1) (interleave lst2 (cdr lst1)))
+        )
+  )
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+
+
+(define (no-repeats s)
+    (if (null? s) '()
+        (cons (car s) (no-repeats (my-filter (lambda (x) (not (= x (car s))))
+                                  (cdr s))))
+    )
+)
+
+; (define (no-repeats s)
+;   (if (null? s)
+;       '()
+;       (cons (car s)
+;             (no-repeats (my-filter (lambda (x) (not (= x (car s)))) 
+;                                    (cdr s))))))
